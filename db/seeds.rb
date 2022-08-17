81fa345c037acef0340d6274d62d6a3cb0f46e7e
Alex Rich <alexanderrich28@gmail.com>
2022-08-17 16:11:03 +0800

    Yelp routes, models, controllers, views created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+5.times do
+  r = Restaurant.create!(name: Faker::Restaurant.name, address: Faker::Address.full_address, category: %w[chinese italian japanese french belgian].sample, phone_number: Faker::PhoneNumber.cell_phone_with_country_code)
+  puts "#{r.name} created"
+end
